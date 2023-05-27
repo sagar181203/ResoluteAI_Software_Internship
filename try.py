dd82acfb87ef1558fab5a34cac410c204fc95ee9
sagar181203 <sagar181203@gmail.com>
2023-05-27 17:06:13 +0530

    From VS code

---
+++ try.py
@@ -0,0 +1,11 @@
+# Trying with the Git Hub
+# factorial of a number
+
+def factorial(n):
+    if n==1:
+        return n
+    else:
+        return n*factorial(n-1)
+    
+num=int(input("Enter the Number: "))
+print("The factorial of ",num,"is : ",factorial(num))
